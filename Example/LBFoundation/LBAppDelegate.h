6cb97b7d6b51a0ecb8c5354627ae732289669922
grizzlyzhao <grizzlyzhao@tencent.com>
2022-03-15 03:08:04 +0800

    Initial commit

---
+++ Example/LBFoundation/LBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LBAppDelegate.h
+//  LBFoundation
+//
+//  Created by grizzlyzhao on 03/15/2022.
+//  Copyright (c) 2022 grizzlyzhao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LBFoundation/LBViewController.h
@@ -0,0 +1,13 @@
+//
+//  LBViewController.h
+//  LBFoundation
+//
+//  Created by grizzlyzhao on 03/15/2022.
+//  Copyright (c) 2022 grizzlyzhao. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LBViewController : UIViewController
+
+@end
